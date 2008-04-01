4a99515be0946a0568344107ff7b7fba723f9d04
rowen <rowen@git.lsstcorp.org>
2008-04-01 23:45:07 +0000

    daf_persistence reorg: - Added package-wide header file

---
+++ include/lsst/daf/persistence.h
@@ -0,0 +1,29 @@
+// -*- lsst-c++ -*-
+#ifndef LSST_DAF_PERSISTENCE_H
+#define LSST_DAF_PERSISTENCE_H
+
+/** @file
+  * @ingroup daf_persistence
+  *
+  * @brief daf_persistence package header file
+  *
+  * @author Kian-Tat Lim, SLAC
+  * @version $Revision: 4654 $
+  * @date $Date: 2008-04-01 14:28:07 -0700 (Tue, 01 Apr 2008) $
+  */
+
+#include <lsst/daf/persistence/BoostStorage.h>
+#include <lsst/daf/persistence/DbAuth.h>
+#include <lsst/daf/persistence/DbStorage.h>
+#include <lsst/daf/persistence/DbStorageLocation.h>
+#include <lsst/daf/persistence/DbTsvStorage.h>
+#include <lsst/daf/persistence/FitsStorage.h>
+#include <lsst/daf/persistence/Formatter.h>
+#include <lsst/daf/persistence/FormatterImpl.h>
+#include <lsst/daf/persistence/LogicalLocation.h>
+#include <lsst/daf/persistence/Persistence.h>
+#include <lsst/daf/persistence/Storage.h>
+#include <lsst/daf/persistence/StorageRegistry.h>
+#include <lsst/daf/persistence/XmlStorage.h>
+
+#endif // #ifndef LSST_DAF_PERSISTENCE_H
