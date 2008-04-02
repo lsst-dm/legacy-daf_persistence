177dfc626ef1b596e9f3bff2009ec492c94a366d
ktlim <ktlim@git.lsstcorp.org>
2008-04-02 19:47:48 +0000

    Remove some headers that people shouldn't typically use.

--- include/lsst/daf/persistence.h
+++ include/lsst/daf/persistence.h
@@ -15,15 +15,12 @@
 #include <lsst/daf/persistence/BoostStorage.h>
 #include <lsst/daf/persistence/DbAuth.h>
 #include <lsst/daf/persistence/DbStorage.h>
-#include <lsst/daf/persistence/DbStorageLocation.h>
 #include <lsst/daf/persistence/DbTsvStorage.h>
 #include <lsst/daf/persistence/FitsStorage.h>
 #include <lsst/daf/persistence/Formatter.h>
 #include <lsst/daf/persistence/FormatterImpl.h>
 #include <lsst/daf/persistence/LogicalLocation.h>
 #include <lsst/daf/persistence/Persistence.h>
-#include <lsst/daf/persistence/Storage.h>
-#include <lsst/daf/persistence/StorageRegistry.h>
 #include <lsst/daf/persistence/XmlStorage.h>
 
 #endif // #ifndef LSST_DAF_PERSISTENCE_H
