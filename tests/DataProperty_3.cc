fa89ca9d42b1b8414c2aa5e0ae42bb40cd237b15
ktlim <ktlim@git.lsstcorp.org>
2008-04-02 19:44:27 +0000

    Fix namespaces.

--- tests/DataProperty_3.cc
+++ tests/DataProperty_3.cc
@@ -1,20 +1,21 @@
 // -*- lsst-c++ -*-
 #include <lsst/daf/base/DataProperty.h>
-#include <lsst/mwi/base/Citizen.h>
-#include <lsst/mwi/utils/Trace.h>
+#include <lsst/daf/base/Citizen.h>
+#include <lsst/pex/logging/Trace.h>
+#include <lsst/utils/Utils.h>
 
-#include "lsst/mwi/policy/Policy.h"
-#include "lsst/mwi/persistence/Persistence.h"
-#include "lsst/mwi/persistence/LogicalLocation.h"
+#include "lsst/pex/policy/Policy.h"
+#include "lsst/daf/persistence/Persistence.h"
+#include "lsst/daf/persistence/LogicalLocation.h"
 
 using namespace std;
-using namespace lsst::mwi::data;
-using namespace lsst::mwi::utils;
+using namespace lsst::daf::base;
+using namespace lsst::pex::logging;
 
 void test() {
-     DataProperty::PtrType additionalData = SupportFactory::createPropertyNode("additionalData"); // empty for testing
+     DataProperty::PtrType additionalData = DataProperty::createPropertyNode("additionalData"); // empty for testing
 
-     DataProperty::PtrType root = SupportFactory::createPropertyNode("root");
+     DataProperty::PtrType root = DataProperty::createPropertyNode("root");
 
      DataProperty::PtrType prop1(new DataProperty("name1", std::string("value1")));
      DataProperty::PtrType prop2(new DataProperty("name2", 2));
@@ -24,40 +25,40 @@ void test() {
      root->addProperty(prop2);
      root->addProperty(prop2a);
 
-     lsst::mwi::policy::Policy::Ptr policyPtr(new lsst::mwi::policy::Policy);
-     lsst::mwi::persistence::Persistence::Ptr persist =
-         lsst::mwi::persistence::Persistence::getPersistence(policyPtr);
-     lsst::mwi::persistence::Storage::List storageList;
+     lsst::pex::policy::Policy::Ptr policyPtr(new lsst::pex::policy::Policy);
+     lsst::daf::persistence::Persistence::Ptr persist =
+         lsst::daf::persistence::Persistence::getPersistence(policyPtr);
+     lsst::daf::persistence::Storage::List storageList;
 
-     lsst::mwi::persistence::LogicalLocation loc("fov391/root.boost");
+     lsst::daf::persistence::LogicalLocation loc("fov391/root.boost");
      storageList.push_back(persist->getPersistStorage("BoostStorage", loc));
      persist->persist(*root, storageList, additionalData);
 
-     lsst::mwi::persistence::LogicalLocation loc2("fov391/root.xml");
+     lsst::daf::persistence::LogicalLocation loc2("fov391/root.xml");
      storageList[0] = persist->getPersistStorage("XmlStorage", loc2);
      persist->persist(*root, storageList, additionalData);
 }     
 
 void test2()
 {
-    DataProperty::PtrType additionalData = SupportFactory::createPropertyNode("additionalData"); // empty for testing
+    DataProperty::PtrType additionalData = DataProperty::createPropertyNode("additionalData"); // empty for testing
 
-    boost::any foo = lsst::mwi::utils::stringToAny("-1234");
-    boost::any foo2 = lsst::mwi::utils::stringToAny("1.234e-1");
-    boost::any foo3 = lsst::mwi::utils::stringToAny("'This is a Fits string'");
+    boost::any foo = lsst::utils::stringToAny("-1234");
+    boost::any foo2 = lsst::utils::stringToAny("1.234e-1");
+    boost::any foo3 = lsst::utils::stringToAny("'This is a Fits string'");
     
     DataProperty::PtrType fooProp(new DataProperty("foo", foo));
     DataProperty::PtrType fooProp2(new DataProperty("foo2", foo2));
     DataProperty::PtrType fooProp3(new DataProperty("foo3", foo3));
 
-    lsst::mwi::policy::Policy::Ptr policyPtr(new lsst::mwi::policy::Policy);
-    lsst::mwi::persistence::Persistence::Ptr persist =
-        lsst::mwi::persistence::Persistence::getPersistence(policyPtr);
-    lsst::mwi::persistence::Storage::List storageList;
+    lsst::pex::policy::Policy::Ptr policyPtr(new lsst::pex::policy::Policy);
+    lsst::daf::persistence::Persistence::Ptr persist =
+        lsst::daf::persistence::Persistence::getPersistence(policyPtr);
+    lsst::daf::persistence::Storage::List storageList;
 
-    lsst::mwi::persistence::LogicalLocation loc("fov391/foo.boost");
-    lsst::mwi::persistence::LogicalLocation loc2("fov391/foo2.boost");
-    lsst::mwi::persistence::LogicalLocation loc3("fov391/foo3.boost");
+    lsst::daf::persistence::LogicalLocation loc("fov391/foo.boost");
+    lsst::daf::persistence::LogicalLocation loc2("fov391/foo2.boost");
+    lsst::daf::persistence::LogicalLocation loc3("fov391/foo3.boost");
 
     storageList.push_back(persist->getPersistStorage("BoostStorage", loc));
     persist->persist(*fooProp, storageList, additionalData);
