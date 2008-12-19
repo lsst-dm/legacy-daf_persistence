3d5ea11d92e7717a69130bc2144dfe7d1cfcc5ab
ktlim <ktlim@git.lsstcorp.org>
2008-12-19 22:56:22 +0000

    Change test to PropertySet from DataProperty. #492

--- tests/Persistence_2.py
+++ tests/Persistence_2.py
@@ -4,23 +4,21 @@ import lsst.pex.policy as pexPolicy
 
 def test1():
     """
-    Test DataProperty persistence to database without policy.
+    Test PropertySet persistence to database without policy.
     """
 
-    dp = dafBase.DataProperty.createPropertyNode("foo")
-    dp.addProperty(dafBase.DataProperty("intField", 1))
-    dp.addProperty(dafBase.DataProperty("doubleField", 1.2))
-    dp.addProperty(dafBase.DataProperty("varcharField", "Testing"))
-    dp.addProperty(dafBase.DataProperty.createBoolDataProperty("boolField", True))
-    dp.addProperty(dafBase.DataProperty("int64Field", 9876543210L))
-    dp.addProperty(dafBase.DataProperty.createFloatDataProperty("floatField", \
-        3.14))
+    dp = dafBase.PropertySet()
+    dp.addInt("intField", 1)
+    dp.addDouble("doubleField", 1.2)
+    dp.addString("varcharField", "Testing")
+    dp.addBool("boolField", True)
+    dp.addInt64("int64Field", 9876543210L)
+    dp.addFloat("floatField", 3.14)
 
     pol = pexPolicy.Policy()
 
-    additionalData = dafBase.DataProperty.createPropertyNode("additionalData")
-    additionalData.addProperty(dafBase.DataProperty( \
-            "itemName", "Persistence_Test_2"))
+    additionalData = dafBase.PropertySet()
+    additionalData.add("itemName", "Persistence_Test_2")
 
     loc = dafPersist.LogicalLocation("mysql://lsst10.ncsa.uiuc.edu:3306/test")
 
@@ -33,28 +31,25 @@ def test1():
 
 def test2():
     """
-    Test DataProperty persistence to database with policy mapping itemName to
+    Test PropertySet persistence to database with policy mapping itemName to
     database table name.
     """
 
-    dp = dafBase.DataProperty.createPropertyNode("foo2")
-    dp.addProperty(dafBase.DataProperty("intField", 2))
-    dp.addProperty(dafBase.DataProperty("doubleField", 2.3))
-    dp.addProperty(dafBase.DataProperty("varcharField", "gnitseT"))
-    dp.addProperty(dafBase.DataProperty.createBoolDataProperty("boolField", \
-            False))
-    dp.addProperty(dafBase.DataProperty("int64Field", \
-            9988776655L))
-    dp.addProperty(dafBase.DataProperty.createFloatDataProperty("floatField", \
-            2.718))
+    dp = dafBase.PropertySet()
+    dp.addInt("intField", 2)
+    dp.addDouble("doubleField", 2.3)
+    dp.addString("varcharField", "gnitseT")
+    dp.addBool("boolField", False)
+    dp.addInt64("int64Field", 9988776655L)
+    dp.addFloat("floatField", 2.718)
 
     pol = pexPolicy.Policy()
     itemPol = pexPolicy.Policy()
     itemPol.set("TableName", "Persistence_Test_2")
-    pol.set("Formatter.DataProperty.testItem", itemPol)
+    pol.set("Formatter.PropertySet.testItem", itemPol)
 
-    additionalData = dafBase.DataProperty.createPropertyNode("additionalData")
-    additionalData.addProperty(dafBase.DataProperty("itemName", "testItem"))
+    additionalData = dafBase.PropertySet()
+    additionalData.add("itemName", "testItem")
 
     loc = dafPersist.LogicalLocation("mysql://lsst10.ncsa.uiuc.edu:3306/test")
 
@@ -67,17 +62,17 @@ def test2():
 
 def test3():
     """
-    Test DataProperty persistence to database with policy mapping itemName to
+    Test PropertySet persistence to database with policy mapping itemName to
     database table name and mapping property keys to table columns.
     """
 
-    dp = dafBase.DataProperty.createPropertyNode("foo3")
-    dp.addProperty(dafBase.DataProperty("i", 3))
-    dp.addProperty(dafBase.DataProperty("d", 3.4))
-    dp.addProperty(dafBase.DataProperty("v", "LastOne"))
-    dp.addProperty(dafBase.DataProperty.createBoolDataProperty("b", True))
-    dp.addProperty(dafBase.DataProperty("I", 9998887776L))
-    dp.addProperty(dafBase.DataProperty.createFloatDataProperty("f", 1.414))
+    dp = dafBase.PropertySet()
+    dp.addInt("i", 3)
+    dp.addDouble("d", 3.4)
+    dp.addString("v", "LastOne")
+    dp.addBool("b", True)
+    dp.addInt64("I", 9998887776L)
+    dp.addFloat("f", 1.414)
 
     pol = pexPolicy.Policy()
     itemPol = pexPolicy.Policy()
@@ -88,10 +83,10 @@ def test3():
     itemPol.add("KeyList", "varcharField=v")
     itemPol.add("KeyList", "doubleField=d")
     itemPol.add("KeyList", "intField=i")
-    pol.set("Formatter.DataProperty.testItem", itemPol)
+    pol.set("Formatter.PropertySet.testItem", itemPol)
 
-    additionalData = dafBase.DataProperty.createPropertyNode("additionalData")
-    additionalData.addProperty(dafBase.DataProperty("itemName", "testItem"))
+    additionalData = dafBase.PropertySet()
+    additionalData.add("itemName", "testItem")
 
     loc = dafPersist.LogicalLocation("mysql://lsst10.ncsa.uiuc.edu:3306/test")
 
