0d56eb6c5d259cf185b4e5bf9c5cf53b956864b4
ktlim <ktlim@git.lsstcorp.org>
2009-03-11 06:14:32 +0000

    Add mappings (static and per-locator) to LogicalLocation (#691), with test.  Add debug output to DbStorageImpl (#696).  Clean up tests/SConscript a little.

---
+++ tests/LogicalLocation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import unittest
+
+from lsst.daf.persistence import LogicalLocation
+from lsst.daf.base import PropertySet
+
+import lsst.pex.logging
+lsst.pex.logging.Trace_setVerbosity("daf.persistence.LogicalLocation", 10)
+
+class LogicalLocationTestCase(unittest.TestCase):
+    """A test case for LogicalLocation."""
+
+    def testSubst(self):
+        ad = PropertySet()
+        ad.set("foo", "bar")
+        ad.setInt("x", 3)
+        LogicalLocation.setLocationMap(ad)
+        l = LogicalLocation("%(foo)xx")
+        self.assertEqual(l.locString(), "barxx")
+        l = LogicalLocation("%(x)foo")
+        self.assertEqual(l.locString(), "3foo")
+
+        ad2 = PropertySet()
+        ad2.set("foo", "baz")
+        ad2.setInt("y", 2009)
+        l = LogicalLocation("%(foo)%(x)%(y)", ad2)
+        self.assertEqual(l.locString(), "bar32009")
+        LogicalLocation.setLocationMap(PropertySet())
+        l = LogicalLocation("%(foo)%(y)", ad2)
+        self.assertEqual(l.locString(), "baz2009")
+
+if __name__ == '__main__':
+    unittest.main()
