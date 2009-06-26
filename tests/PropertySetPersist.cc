3045a64432e12f348bceba4aea491791d72b450b
ktlim <ktlim@git.lsstcorp.org>
2009-06-26 21:01:06 +0000

    Add more value types to Boost/XML serialization of PropertySet.  Reorganize XML serialization to be more human-understandable.  Add tests for new types.  Update doxygen.conf.

--- tests/PropertySetPersist.cc
+++ tests/PropertySetPersist.cc
@@ -4,9 +4,11 @@
 #include <lsst/pex/logging/Trace.h>
 #include <lsst/utils/Utils.h>
 
+#include "boost/make_shared.hpp"
 #include "lsst/pex/policy/Policy.h"
 #include "lsst/daf/persistence/Persistence.h"
 #include "lsst/daf/persistence/LogicalLocation.h"
+#include "lsst/daf/base/DateTime.h"
 
 #define BOOST_TEST_MODULE dafBase::PropertySetPersist
 #include "boost/test/included/unit_test.hpp"
@@ -66,6 +68,77 @@ BOOST_AUTO_TEST_CASE(PersistDifferentTypes) {
 
     storageList[0] = persist->getPersistStorage("BoostStorage", loc3);
     persist->persist(*fooProp3, storageList, additionalData);
+}
+
+BOOST_AUTO_TEST_CASE(PersistManyTypes) {
+    dafBase::PropertySet::Ptr additionalData =
+        boost::make_shared<dafBase::PropertySet>(); // empty for testing
+
+    dafBase::PropertySet::Ptr fooProp =
+        boost::make_shared<dafBase::PropertySet>();
+    fooProp->set<char>("char", 'x');
+    fooProp->set<signed char>("schar", 'y');
+    fooProp->set<unsigned char>("uchar", 'z');
+    fooProp->set<short>("short", -2009);
+    fooProp->set<unsigned short>("ushort", 40109);
+    fooProp->set("str", "Testing");
+    fooProp->set<float>("float", 3.14159f);
+    fooProp->set<double>("dbl", 2.718281828459045);
+    dafBase::DateTime now = dafBase::DateTime::now();
+    fooProp->set("date", now);
+
+    lsst::pex::policy::Policy::Ptr policyPtr =
+        boost::make_shared<lsst::pex::policy::Policy>();
+    dafPersist::Persistence::Ptr persist =
+        dafPersist::Persistence::getPersistence(policyPtr);
+
+    dafPersist::LogicalLocation loc("tests/data/many.boost");
+    dafPersist::LogicalLocation loc2("tests/data/many.xml");
+
+    dafPersist::Storage::List storageList;
+    storageList.push_back(persist->getPersistStorage("BoostStorage", loc));
+    persist->persist(*fooProp, storageList, additionalData);
+
+    storageList[0] = persist->getPersistStorage("XmlStorage", loc2);
+    persist->persist(*fooProp, storageList, additionalData);
+
+    storageList[0] = persist->getRetrieveStorage("BoostStorage", loc);
+    dafBase::Persistable::Ptr pp =
+        persist->retrieve("PropertySet", storageList, additionalData);
+    BOOST_CHECK(pp);
+    BOOST_CHECK(typeid(*pp) == typeid(dafBase::PropertySet));
+    dafBase::PropertySet::Ptr ps =
+        boost::dynamic_pointer_cast<dafBase::PropertySet,
+                                    dafBase::Persistable>(pp);
+    BOOST_CHECK(ps);
+    BOOST_CHECK(ps.get() != fooProp.get());
+    BOOST_CHECK_EQUAL(ps->get<char>("char"), 'x');
+    BOOST_CHECK_EQUAL(ps->get<signed char>("schar"), 'y');
+    BOOST_CHECK_EQUAL(ps->get<unsigned char>("uchar"), 'z');
+    BOOST_CHECK_EQUAL(ps->get<short>("short"), -2009);
+    BOOST_CHECK_EQUAL(ps->get<unsigned short>("ushort"), 40109);
+    BOOST_CHECK_EQUAL(ps->get<std::string>("str"), "Testing");
+    BOOST_CHECK_EQUAL(ps->get<float>("float"), 3.14159f);
+    BOOST_CHECK_EQUAL(ps->get<double>("dbl"), 2.718281828459045);
+    BOOST_CHECK_EQUAL(ps->get<dafBase::DateTime>("date").nsecs(), now.nsecs());
+
+    storageList[0] = persist->getRetrieveStorage("XmlStorage", loc2);
+    pp = persist->retrieve("PropertySet", storageList, additionalData);
+    BOOST_CHECK(pp);
+    BOOST_CHECK(typeid(*pp) == typeid(dafBase::PropertySet));
+    ps = boost::dynamic_pointer_cast<dafBase::PropertySet,
+                                     dafBase::Persistable>(pp);
+    BOOST_CHECK(ps);
+    BOOST_CHECK(ps.get() != fooProp.get());
+    BOOST_CHECK_EQUAL(ps->get<char>("char"), 'x');
+    BOOST_CHECK_EQUAL(ps->get<signed char>("schar"), 'y');
+    BOOST_CHECK_EQUAL(ps->get<unsigned char>("uchar"), 'z');
+    BOOST_CHECK_EQUAL(ps->get<short>("short"), -2009);
+    BOOST_CHECK_EQUAL(ps->get<unsigned short>("ushort"), 40109);
+    BOOST_CHECK_EQUAL(ps->get<std::string>("str"), "Testing");
+    BOOST_CHECK_EQUAL(ps->get<float>("float"), 3.14159f);
+    BOOST_CHECK_EQUAL(ps->get<double>("dbl"), 2.718281828459045);
+    BOOST_CHECK_EQUAL(ps->get<dafBase::DateTime>("date").nsecs(), now.nsecs());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
