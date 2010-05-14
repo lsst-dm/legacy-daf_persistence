e82078bfc963e8dd07e86582a58d382b4c5f8731
ktlim <ktlim@git.lsstcorp.org>
2010-05-14 04:51:19 +0000

    Reverse most of [14994], which was incorrect.

--- python/lsst/daf/persistence/butler.py
+++ python/lsst/daf/persistence/butler.py
@@ -96,10 +96,6 @@ class Butler(object):
 
         dataId = self._combineDicts(dataId, **rest)
         location = self.mapper.map(datasetType, dataId)
-        if location == []:
-            raise RuntimeError, \
-                    "No location for dataset type %s, keys = %s" % \
-                    (datasetType, str(dataId))
         additionalData = location.getAdditionalData()
         storageName = location.getStorageName()
         if storageName in ('BoostStorage', 'FitsStorage', 'PafStorage',
@@ -124,10 +120,6 @@ class Butler(object):
         """
         dataId = self._combineDicts(dataId, **rest)
         location = self.mapper.map(datasetType, dataId)
-        if location == []:
-            raise RuntimeError, \
-                    "No location for dataset type %s, keys = %s" % \
-                    (datasetType, str(dataId))
 
         if location.getPythonType() is not None:
             # import this pythonType dynamically 
