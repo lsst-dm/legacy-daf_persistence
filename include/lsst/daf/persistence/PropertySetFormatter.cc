c07b65e79d3904bc7672569d1178870c291ccf08
Russell Owen <rowen@uw.edu>
2014-06-25 15:55:11 -0700

    Respond to K-T's review by removing trailing whitespace in two files.

--- include/lsst/daf/persistence/PropertySetFormatter.cc
+++ include/lsst/daf/persistence/PropertySetFormatter.cc
@@ -1,9 +1,9 @@
 // -*- lsst-c++ -*-
 
-/* 
+/*
  * LSST Data Management System
  * Copyright 2008, 2009, 2010 LSST Corporation.
- * 
+ *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
  *
@@ -11,17 +11,17 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the LSST License Statement and 
- * the GNU General Public License along with this program.  If not, 
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
  * see <http://www.lsstcorp.org/LegalNotices/>.
  */
- 
+
 
 /** \file
  * \brief Implementation of PropertySetFormatter class
@@ -121,7 +121,7 @@ void PropertySetFormatter::delegateSerialize(
     }
     ar & make_nvp("base",
                   boost::serialization::base_object<dafBase::Persistable>(*ps));
-    
+
     std::vector<std::string> names;
     size_t nNames;
     if (Archive::is_saving::value) {
@@ -182,7 +182,7 @@ void PropertySetFormatter::delegateSerialize(
         case 'T': serializeDateTime<Archive>(ar, name, ps); break;
         case 'p': serializeItem<Archive, dafBase::Persistable::Ptr>(ar, name, ps); break;
         default:
-                  throw LSST_EXCEPT(lsst::pex::exceptions::RuntimeError, 
+                  throw LSST_EXCEPT(lsst::pex::exceptions::RuntimeError,
                       std::string("Unknown type reading PropertySet") +
                       type + ", name = " + name);
         }
