0872c9993f5cac31922961dcb0a9c524717f7acb
Nate Pease <npease@slac.stanford.edu>
2016-03-11 17:44:40 -0800

    updates Cfg mechanism in repositoryMapper
    also adds the license file

--- python/lsst/daf/persistence/repositoryMapper.py
+++ python/lsst/daf/persistence/repositoryMapper.py
@@ -1,10 +1,33 @@
+#!/usr/bin/env python
 
+#
+# LSST Data Management System
+# Copyright 2016 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
 
-from lsst.daf.persistence import Mapper, Policy, ButlerLocation
+import yaml
 
-#<?cfg paf policy ?>
+from lsst.daf.persistence import Mapper, Policy, ButlerLocation, MapperCfg
 
-
+class RepositoryMapperCfg(MapperCfg):
+    pass
 
 class RepositoryMapper(Mapper):
 
@@ -17,7 +40,7 @@ class RepositoryMapper(Mapper):
 
     @classmethod
     def cfg(cls, policy=None, access=None):
-        return Policy({'cls':cls, 'policy':policy, 'access':access})
+        return RepositoryMapperCfg(cls=cls, policy=policy, access=access)
 
     def __repr__(self):
         if 'policy' in self.__dict__ and 'access' in self.__dict__:
