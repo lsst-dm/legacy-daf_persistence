4dd5be8daa7a9a425dd0c8e6462fdf4a4c070372
Pim Schellart <P.Schellart@princeton.edu>
2016-07-26 15:52:41 -0400

    Wrap with pybind11 instead of swig

---
+++ python/lsst/daf/persistence/dbAuth.cc
@@ -0,0 +1,24 @@
+#include <pybind11/pybind11.h>
+
+#include "lsst/daf/persistence/DbAuth.h"
+
+using namespace lsst::daf::persistence;
+
+namespace py = pybind11;
+
+PYBIND11_DECLARE_HOLDER_TYPE(MyType, std::shared_ptr<MyType>);
+
+PYBIND11_PLUGIN(_dbAuth) {
+    py::module mod("_dbAuth", "Access to the classes from the daf_persistence dbAuth library");
+
+    py::class_<DbAuth> cls(mod, "DbAuth");
+
+    cls.def_static("setPolicy", &DbAuth::setPolicy);
+    cls.def_static("available", &DbAuth::available);
+    cls.def_static("authString", &DbAuth::authString);
+    cls.def_static("username", &DbAuth::username);
+    cls.def_static("password", &DbAuth::password);
+
+    return mod.ptr();
+}
+
---
+++ python/lsst/daf/persistence/persistenceLib.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+import lsst.daf.base
+
+from ._logicalLocation import *
+from ._persistence import *
+from ._storage import *
+from ._dbAuth import *
+
+StorageList = list
