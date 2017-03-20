289ea91e51e2c93b6c65ee8d8e961396fec91e39
Nate Pease <n8pease@dhcp-visitor-enr-118-103.slac.stanford.edu>
2017-03-20 14:48:09 -0700

    rename the C++ Storage class to StorageFormatter

--- src/XmlStorage.cc
+++ src/XmlStorage.cc
@@ -1,9 +1,9 @@
 // -*- lsst-c++ -*-
 
-/* 
+/*
  * LSST Data Management System
  * Copyright 2008, 2009, 2010 LSST Corporation.
- * 
+ *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
  *
@@ -11,17 +11,17 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the LSST License Statement and 
- * the GNU General Public License along with this program.  If not, 
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
  * see <http://www.lsstcorp.org/LegalNotices/>.
  */
- 
+
 
 /** \file
  * \brief Implementation of XmlStorage class
@@ -51,7 +51,7 @@ namespace persistence {
 
 /** Constructor.
  */
-XmlStorage::XmlStorage(void) : Storage(typeid(*this)),
+XmlStorage::XmlStorage(void) : StorageFormatter(typeid(*this)),
     _ostream{}, _istream{}, _oarchive{}, _iarchive{} {
 }
 
@@ -62,7 +62,7 @@ XmlStorage::XmlStorage(void) : Storage(typeid(*this)),
 XmlStorage::~XmlStorage(void) {
 }
 
-/** Allow a Policy to be used to configure the Storage.
+/** Allow a Policy to be used to configure the StorageFormatter.
  * \param[in] policy
  */
 void XmlStorage::setPolicy(lsst::pex::policy::Policy::Ptr policy) {
