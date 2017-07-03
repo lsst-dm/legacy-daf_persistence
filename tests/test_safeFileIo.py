38f16155b65c134d8aeb257d06186fa04b3f3d6a
Nathan Pease <npease@slac.stanford.edu>
2017-07-03 13:54:57 -0700

    merge the two safeFileIO tests

--- tests/test_safeFileIo.py
+++ tests/test_safeFileIo.py
@@ -22,16 +22,22 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 
+import multiprocessing
 import os
 import shutil
 import stat
+import time
 import unittest
 
 import lsst.daf.persistence as dp
+from lsst.utils import getPackageDir
 import lsst.utils.tests
+from lsst.log import Log
+
 
 # Define the root of the tests relative to this file
 ROOT = os.path.abspath(os.path.dirname(__file__))
+packageDir = os.path.join(getPackageDir('daf_persistence'))
 
 
 def setup_module(module):
@@ -89,6 +95,114 @@ class WriteOnceCompareSameTest(unittest.TestCase):
 
         filePerms = stat.S_IMODE(os.lstat(fileName).st_mode)
         self.assertEqual(~umask & 0o666, filePerms)
+
+
+def readFile(filename, readQueue):
+    readQueue.put("waiting")
+    readQueue.get()
+    with dp.safeFileIo.SafeLockedFileForRead(filename) as f:
+        readQueue.put(f.read())
+
+
+class TestFileLocking(unittest.TestCase):
+    """A test case for safeFileIo file read and write locking"""
+
+    testDir = os.path.join(packageDir, 'tests', 'TestFileLocking')
+
+    def setUp(self):
+        if os.path.exists(self.testDir):
+            shutil.rmtree(self.testDir)
+
+    def tearDown(self):
+        if os.path.exists(self.testDir):
+            shutil.rmtree(self.testDir)
+
+    def testWriteLock(self):
+        """Test SafeLockedFileForWrite by
+        1. open a file for write
+        2. spawn a second process that tries to read the file but should be blocked by the file lock
+        3. then write the file it and closing it (in the first process)
+        4. the second process should then be unblocked
+        5. read the file in the second process and return the result to the first process
+        6. compare what was written and read
+        """
+        readQueue = multiprocessing.Queue()
+        fileName = os.path.join(self.testDir, "testfile.txt")
+        proc = multiprocessing.Process(target=readFile, args=(fileName, readQueue))
+        testStr = "foobarbaz"
+        proc.start()
+        self.assertEqual(readQueue.get(), "waiting")
+        with dp.safeFileIo.SafeLockedFileForWrite(fileName) as f:
+            readQueue.put("go")
+            time.sleep(1)
+            f.write(testStr)
+        self.assertEqual(readQueue.get(), testStr)
+        proc.join()
+
+    def testNoChange(self):
+        """Test that if a file is opened and not changed that the file does not get changed"""
+        fileName = os.path.join(self.testDir, "testfile.txt")
+        # create the file with some contents
+        with dp.safeFileIo.SafeLockedFileForWrite(fileName) as f:
+            f.write("some test string")
+        # open the file but do not change it
+        with dp.safeFileIo.SafeLockedFileForWrite(fileName) as f:
+            pass
+        # open the file for read and test that it still contains the original test contents
+        with dp.safeFileIo.SafeLockedFileForRead(fileName) as f:
+            self.assertEqual(f.read(), "some test string")
+
+
+class TestOneThousandWriters(unittest.TestCase):
+    """Test for efficient file updating with shared & exclusive locks by serializing a RepositoryCfg to a
+    location 1000 times. When this was tested on a 2.8 GHz Intel Core i7 macbook pro it took about 1.3 seconds
+    to run. When the squash performance monitoring framework is done, this test could be monitored in that
+    system."""
+
+    testDir = os.path.join(packageDir, 'tests', 'TestOneThousandWriters')
+
+    def setUp(self):
+        if os.path.exists(self.testDir):
+            shutil.rmtree(self.testDir)
+
+    def tearDown(self):
+        if os.path.exists(self.testDir):
+            shutil.rmtree(self.testDir)
+
+    @staticmethod
+    def writeCfg(cfg, go):
+        while go is False:
+            pass
+        dp.PosixStorage.putRepositoryCfg(cfg)
+
+    def testWriteCfg(self):
+        # The number of writers to use can result in too many open files
+        # We calculate this as the 80% of the maximum allowed number for this
+        # process, or 1000, whichever is smaller.
+        numWriters = 1000
+        try:
+            import resource
+            limit = resource.getrlimit(resource.RLIMIT_NOFILE)
+            allowedOpen = int(limit[0] * 0.8)
+            if allowedOpen < numWriters:
+                numWriters = allowedOpen
+        except:
+            # Use the default number if we had trouble obtaining resources
+            pass
+        startTime = time.time()
+        go = multiprocessing.Value('b', False)
+        cfg = dp.RepositoryCfg(root=os.path.join(TestOneThousandWriters.testDir), mapper='bar', mapperArgs={},
+                               parents=None, policy=None)
+        procs = [multiprocessing.Process(target=TestOneThousandWriters.writeCfg, args=(cfg, go))
+                 for x in range(numWriters)]
+        for proc in procs:
+            proc.start()
+        go = True
+        for proc in procs:
+            proc.join()
+        endTime = time.time()
+        log = Log.getLogger("daf.persistence")
+        log.trace("TestOneThousandWriters took {} seconds.".format(endTime-startTime))
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
