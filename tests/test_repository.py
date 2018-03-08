12ca6933536a5cb463032e1b17329328f85e782c
Eli Rykoff <erykoff@slac.stanford.edu>
2018-03-07 16:16:30 -0800

    Fix getUri test so that it only uses raw datatypes

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -179,9 +179,9 @@ class TestBasics(unittest.TestCase):
         self.assertEqual(os.path.isfile(raw_uri), True)
 
     def testGetUriWrite(self):
-        raw_uri = self.butler.getUri('src', {'visit': '9', 'filter': 'g'}, write=True)
+        raw_uri = self.butler.getUri('raw', {'visit': '9', 'filter': 'g'}, write=True)
         self.assertEqual(raw_uri,
-                         os.path.join(self.testDir, 'repoA', 'raw', 'raw_v9_fg.fits'))
+                         os.path.join(self.testDir, 'repoA', 'raw', 'raw_v9_fg.fits.gz'))
         self.assertEqual(os.path.isfile(raw_uri), False)
 
     def testSubset(self):
