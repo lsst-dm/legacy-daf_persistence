be75d3d759a2dc1a811abd2de23dbfa6cbbb6b4d
Timothy Morton <tdm@astro.princeton.edu>
2018-05-03 13:45:31 -0500

    remove warning for multiple results

--- python/lsst/daf/persistence/posixStorage.py
+++ python/lsst/daf/persistence/posixStorage.py
@@ -649,9 +649,6 @@ def readParquetStorage(butlerLocation):
         #  filename should be the first kwarg, but being explicit here.
         results.append(pythonType(filename=filename))
 
-    if len(results) > 1:
-        Log.getLogger("daf.persistence.butler").warning('Not using multiple locations!')
-
     return results
 
 
